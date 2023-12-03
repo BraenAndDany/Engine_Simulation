6cd6bd8d1d7fad2cbe07524ed6eb767be2f14556
BraenAndDany <144007672+BraenAndDany@users.noreply.github.com>
2023-12-03 21:22:04 +0700

    add

--- Engine_Simulation/Engine.cs
+++ Engine_Simulation/Engine.cs
@@ -14,7 +14,7 @@ namespace Engine
             //сдесь сохранены все уже заранее заданные значения
             public int Engine_Inertia = 10;
             public double[] Rotational_Moment = {20,75,100,105,75,0};
-            public double[] Crankshaft_Rotation_Speed = { 0, 75, 150, 200, 250, 300 };
+            public double[] Crankshaft_Rotation_Speed = {0,75,150,200,250,300};
             public int Overheating_Temperature = 110;
             public double Dependency_Ratio_Hm = 0.01;
             public double Dependency_Ratio_Hv = 0.0001;
@@ -25,9 +25,9 @@ namespace Engine
                 double Heating_Speed = (Rotational_Moment * Dependency_Ratio_Hm) + (Math.Pow(Crankshaft_Rotation_Speed,2) * Dependency_Ratio_Hv);
                 return Heating_Speed;
             }
-            public double Engine_Cooling_Rate(double Ambient_Temperature,double Engine_Temperature,double Dependency_Ratio_C)
+            public double Engine_Cooling_Rate(double Ambient_Temperature,double Engine_Temperature,double Dependency_Ratio_C,double second)
             {
-                double Cooling_Rate = Dependency_Ratio_C*(Ambient_Temperature-Engine_Temperature);
+                double Cooling_Rate = Dependency_Ratio_C*(Ambient_Temperature-Engine_Temperature/3.56);
                 return Cooling_Rate;
             }
             public double Engine_Power(double Rotational_Moment, double Crankshaft_Rotation_Speed)
@@ -35,25 +35,15 @@ namespace Engine
                 double engine_power = Rotational_Moment * Crankshaft_Rotation_Speed / 1000;
                 return engine_power;
             }
-            public double Engine_Temperature(double Engine_Temperature, double Engine_Heating, double Engine_Cooling)
+            public double Engine_Temperature(double Engine_Temperature, double Engine_Heating, double Engine_Cooling, double Ambient_Temperature,double second)
             {
-                Engine_Temperature=Engine_Temperature+Engine_Heating-Engine_Cooling;
+                Engine_Temperature=Ambient_Temperature+(Engine_Heating+Engine_Cooling)*second;
                 return Engine_Temperature;
             }
-            public double Hm(double Rotational_Moment, double Engine_Heating)
+            public double C(double Engine_Temperature, double Ambient_Temperature, double Engine_Cooling_Rate)
             {
-                double Hm = Rotational_Moment / Engine_Heating;
-                return Hm;
-            }
-            public double Hv(double Crankshaft_Rotation_Speed, double Engine_Heating)
-            {
-                double Hv = Crankshaft_Rotation_Speed / Engine_Heating;
-                return Hv;
-            }
-            public double C(double Engine_cooling, double Ambient_Temperature, double Engine_Temperature)
-            {
-              double C = Engine_cooling/(Ambient_Temperature-Engine_Temperature);
-              return C;
+                double C = Engine_Cooling_Rate / (Ambient_Temperature - Engine_Temperature);
+                return C;
             }
         }
 
--- Engine_Simulation/Program.cs
+++ Engine_Simulation/Program.cs
@@ -9,42 +9,50 @@ namespace Engine_Simulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the ambient temperature");
-            Enternal_Combustion_Engine engine_stats =new Enternal_Combustion_Engine();
-            double Ambient_Temperature=Convert.ToDouble(Console.ReadLine());
-            //инициализация переменных для манипуляции с ними
-            double boost_M, boost_V, Rotational_Moment = engine_stats.Rotational_Moment[0], Crankshaft_Rotation_Speed = engine_stats.Crankshaft_Rotation_Speed[0], Dependency_Ratio_Hm=engine_stats.Dependency_Ratio_Hm, Dependency_Ratio_Hv=engine_stats.Dependency_Ratio_Hv, Dependency_Ratio_C=engine_stats.Dependency_Ratio_C;
-            int seconds = 0, array_counter_M=1 ,array_counter_V = 0;
-            double Engine_Temperature = Ambient_Temperature;
-            double Engine_Heating,Engine_Cooling,Hm, Hv,Engine_Power;
-            //цикл который симулирует график приведённый в задании
-            while (true)
+            try
             {
-                if (engine_stats.Rotational_Moment[array_counter_M]<=Rotational_Moment)
+                Console.WriteLine("Please enter the ambient temperature");
+                Enternal_Combustion_Engine engine_stats = new Enternal_Combustion_Engine();
+                double Ambient_Temperature = Convert.ToDouble(Console.ReadLine());
+                //инициализация переменных для манипуляции с ними
+                double boost_M, boost_V, Rotational_Moment = engine_stats.Rotational_Moment[0], Crankshaft_Rotation_Speed = engine_stats.Crankshaft_Rotation_Speed[0], Dependency_Ratio_Hm = engine_stats.Dependency_Ratio_Hm, Dependency_Ratio_Hv = engine_stats.Dependency_Ratio_Hv, Dependency_Ratio_C = engine_stats.Dependency_Ratio_C;
+                int seconds = 0, array_counter_M = 1, array_counter_V = 0;
+                double Engine_Temperature = Ambient_Temperature;
+                double Engine_Heating, Engine_Cooling, Engine_Power;
+                //цикл который симулирует график приведённый в задании
+                while (true)
                 {
-                    array_counter_M++;
-                    array_counter_V++;
-                }   
-                boost_M = engine_stats.Rotational_Moment[array_counter_M]/engine_stats.Crankshaft_Rotation_Speed[array_counter_M];
-                boost_V = engine_stats.Rotational_Moment[array_counter_V] / engine_stats.Engine_Inertia;
-                Rotational_Moment = Rotational_Moment + boost_M;
-                Crankshaft_Rotation_Speed = Crankshaft_Rotation_Speed + boost_V;
-                Engine_Heating = engine_stats.Engine_Heating_Speed(Rotational_Moment,Crankshaft_Rotation_Speed,Dependency_Ratio_Hm,Dependency_Ratio_Hv);
-                Engine_Cooling = engine_stats.Engine_Cooling_Rate(Ambient_Temperature,Engine_Temperature,Dependency_Ratio_C);
-                Hm = engine_stats.Hm(Rotational_Moment, Engine_Heating);
-                Hv = engine_stats.Hm(Crankshaft_Rotation_Speed, Engine_Heating);
-                Engine_Temperature = engine_stats.Engine_Temperature(Engine_Temperature,Engine_Heating,Engine_Cooling);
-                Dependency_Ratio_C=engine_stats.C(Engine_Cooling,Ambient_Temperature,Engine_Temperature);
-                Engine_Power = Rotational_Moment * Crankshaft_Rotation_Speed / 1000;
-                seconds++;
-                //Вывод результатов при достижении максимально допустимой температуры
-                if(Engine_Temperature>=engine_stats.Overheating_Temperature)
-                {
-                    Console.WriteLine("Time to overheating:"+ seconds+"sec");
-                    Console.WriteLine("Maximum power:"+Engine_Power+ "kW");
-                    Console.WriteLine("The speed of rotation of the crankshaft at this moment:"+Crankshaft_Rotation_Speed+ "radians/sec");
-                    break;
+                    if (engine_stats.Rotational_Moment[array_counter_M] <= Rotational_Moment)
+                    {
+                        array_counter_V++;
+                        array_counter_M++;
+                    }
+                    boost_V = engine_stats.Rotational_Moment[array_counter_V] / engine_stats.Engine_Inertia;
+                    boost_M = (engine_stats.Rotational_Moment[array_counter_M] - engine_stats.Rotational_Moment[array_counter_M - 1]) / ((engine_stats.Crankshaft_Rotation_Speed[array_counter_M] - engine_stats.Crankshaft_Rotation_Speed[array_counter_M - 1]) / boost_V);
+                    Rotational_Moment = Rotational_Moment + boost_M;
+                    Crankshaft_Rotation_Speed = Crankshaft_Rotation_Speed + boost_V;
+                    Engine_Heating = engine_stats.Engine_Heating_Speed(Rotational_Moment, Crankshaft_Rotation_Speed, Dependency_Ratio_Hm, Dependency_Ratio_Hv);
+                    Engine_Cooling = engine_stats.Engine_Cooling_Rate(Ambient_Temperature, Engine_Temperature, Dependency_Ratio_C, seconds);
+                    Engine_Temperature = engine_stats.Engine_Temperature(Engine_Temperature, Engine_Heating, Engine_Cooling, Ambient_Temperature, seconds);
+                    Engine_Power = Rotational_Moment * Crankshaft_Rotation_Speed / 1000;
+                    seconds++;
+                    //Вывод результатов при достижении максимально допустимой температуры
+                    if (Engine_Temperature >= engine_stats.Overheating_Temperature)
+                    {
+                        Console.WriteLine("Time to overheating:" + seconds + "sec");
+                        Console.WriteLine("Maximum power:" + Engine_Power + "kW");
+                        Console.WriteLine("The speed of rotation of the crankshaft at this moment:" + Crankshaft_Rotation_Speed + "radians/sec");
+                        break;
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid value entered");
+            }
+            finally
+            {
+                Console.WriteLine("Restart the program");
             }
         }
     }
